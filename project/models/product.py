d960c6e6bd8e6c7abaa9f2ccf19fa2358d8d70ac
Gadzet <saygitovg@gmail.com>
2024-11-28 12:27:06 +0300

    hw1 completed

--- project/models/product.py
+++ project/models/product.py
@@ -1,25 +1,24 @@
 class Product:
-    def __init__(self, name, category, price):
-        self.name = name
-        self.category = category
-        self.price = price
-        self.sale = 0
+    def __init__(self, name: str, category: str, price: float):
+        self.name: str = name
+        self.category: str = category
+        self.price: float = price
+        self.sale: float = 0
 
-    def edit_category(self, new_category):
-        pass
+    def edit_category(self, new_category: str):
+        self.category = new_category
 
-    def edit_price(self, new_price):
-        pass
+    def edit_price(self, new_price: float):
+        self.price = new_price
 
-    def set_sale(self, sale):
-        pass
+    def set_sale(self, sale: float):
+        self.sale = sale
 
     def cancel_sale(self):
-        pass
+        self.sale = 0
 
-    def get_price(self):
-        # Это не тупо геттер - тут надо учесть скидку и еще то, что скидка указана в процентах
-        pass
+    def get_price(self) -> float:
+        return self.price * (1 - self.sale / 100)
 
-    def __repr__(self):
-        pass
+    def __repr__(self) -> str:
+        return f"<Product {self.name}>"
